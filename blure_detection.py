7ce1bde2210366b0378298a9a6fe8bbf3e38375d
MasoudMoeini <Masoud.Moeini@Studium.uni-hamburg.de>
2022-06-29 00:45:19 +0200

    update blur detection

--- blure_detection.py
+++ blure_detection.py
@@ -3,29 +3,25 @@ from tensorflow import keras
 from keras import layers
 from keras.models import load_model
 from PIL import Image
-
-def pred_num(pred_val):
-  for i in range(10):
-    if (pred_val[i] <0.5):
-      pred_val[i]=0
-    else:
-      pred_val[i]=1
-      num=i
-  return num
-
+import cv2
 
 def prediction(file):
   model = load_model("new_model.h5")
   image = Image.open(file)
   image = image.resize((224,224))
+  img= cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
   image= np.asarray(image).astype(np.float32)/ 255.0
-  #image= np.asarray(Image.open(file)).astype(np.float32)/ 255.0
   image = np.expand_dims(image, -1)
   image = np.expand_dims(image, 0)
   pred = model.predict(image)
   predicted_class = ((pred[1])[0])[0]
   predicted_label = (((pred[0])[0])*255.0).astype(np.uint8)
-  #predicted_label = np.where(predicted_label==255, img_cyan, img)
+  #generating mask from predicted label
+  cyan = np.full_like(img,(0,255,221))
+  blend = 0.5
+  img_cyan = cv2.addWeighted(img, blend, cyan, 1-blend, 0)
+  predicted_label = np.where(predicted_label>=125, img_cyan, img)
+  predicted_label =cv2.cvtColor(predicted_label , cv2.COLOR_BGR2RGB)
   predicted_label = Image.fromarray(predicted_label)
   print(predicted_class)
   if (predicted_class>0.5):
